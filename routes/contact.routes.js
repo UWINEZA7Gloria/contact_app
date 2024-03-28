59191a614d73c27af9149f41d5a2f9a7ff78c657
UWINEZA7Gloria <gloria7wineza@gmail.com>
2024-03-28 09:57:16 +0200

    changes

--- routes/contact.routes.js
+++ routes/contact.routes.js
@@ -1,6 +1,6 @@
 // Contact routes here
 import  express  from "express";
-import contactControllers from "../controllers/contactController.js";
+import contactControllers from "../controllers/contact.controller.js";
 const router=express.Router()
 router.route('/contacts').get(contactControllers.getContacts).post(contactControllers.createContact)
 router.route('/contacts/:id').patch(contactControllers.UpdateContact).delete(contactControllers.deleteContact).get(contactControllers.getContactById)
