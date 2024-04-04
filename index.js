d83b3f2b5d19827b0dea9911c7c37a9201221c13
UWINEZA7Gloria <gloria7wineza@gmail.com>
2024-04-04 09:05:11 +0200

    updating

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
-const app = express();
+import swagger from "./docs/swagger.json" assert{type:"json"}
+import swaggerUi from "swagger-ui-express"
 import configuration from './configs/index.js';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import router from './routes/contact.routes.js';
+const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(router)
+app.use("/contact-docs", swaggerUi.serve, swaggerUi.setup(swagger));
 mongoose.connect(configuration.mongoURI)
 .then(() => {
     app.listen(configuration.port, ()=> {
